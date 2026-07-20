09c80d82a6f814a5e158ee35b8dcd768e4c87abe
Soukaina Aboud <soukiaboudsnap@gmail.com>
2026-07-20 11:45:09 +0200

    Setup Breeze React authentication, role management, and project structure

---
+++ resources/js/Pages/supervisor/dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Supervisor Dashboard</h1>
+            <p>Welcome Supervisor</p>
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,70 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+// Route::get('/', function () {
+//     return redirect()->route('login');
+// });
+Route::get('/force-logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect('/login');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+
+    if (auth()->check()) {
+
+        if (auth()->user()->role === 'admin') {
+            return redirect()->route('admin.dashboard');
+        }
+
+        if (auth()->user()->role === 'supervisor') {
+            return redirect()->route('supervisor.dashboard');
+        }
+    }
+
+    return redirect()->route('login');
 });
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/admin/dashboard', function () {
+        
+        return Inertia::render('Admin/Dashboard');
+    })->name('admin.dashboard');
+
+
+    Route::get('/supervisor/dashboard', function () {
+        return Inertia::render('Supervisor/Dashboard');
+    })->name('supervisor.dashboard');
+
+
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+
+});
+
+
+require __DIR__.'/auth.php';
